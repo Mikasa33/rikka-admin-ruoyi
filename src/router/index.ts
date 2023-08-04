6a501b9d453a2af2882429bfec4be56f278e96d0
Mikasa33 <mikasa33@qq.com>
2023-08-04 16:22:33 +0800

    feat: 配置路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: () => import('@/layouts/home.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'Home',
+          component: () => import('@/views/Home.vue'),
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/',
+      component: () => import('@/layouts/default.vue'),
+      children: [
+        {
+          path: '/about',
+          name: 'About',
+          component: () => import('@/views/About.vue'),
+        },
+        {
+          path: '/hi/:name',
+          name: 'Hi',
+          component: () => import('@/views/Hi.vue'),
+        },
+      ],
+    },
+    {
+      path: '/404',
+      component: () => import('@/layouts/404.vue'),
+      children: [
+        {
+          path: '/:pathMatch(.*)*',
+          name: '404',
+          component: () => import('@/views/404.vue'),
+        },
+      ],
     },
   ],
 })
