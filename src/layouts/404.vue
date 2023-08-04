035c477f9e9d756d8e6b346fe703069cce376f27
Mikasa33 <mikasa33@qq.com>
2023-08-04 16:19:48 +0800

    feat: 添加布局

---
+++ src/layouts/404.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const router = useRouter()
+const { t } = useI18n()
+</script>
+
+<template>
+  <main class="px-4 py-10 text-center text-teal-700 dark:text-gray-200">
+    <div class="text-4xl">
+      <div class="i-carbon-warning inline-block" />
+    </div>
+    <RouterView />
+    <div>
+      <button
+        class="text-sm m-3 mt-8 px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50"
+        @click="router.back()"
+      >
+        {{ t('button.back') }}
+      </button>
+    </div>
+  </main>
+</template>
