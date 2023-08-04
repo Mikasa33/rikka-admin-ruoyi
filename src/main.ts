a91e7c7831050ed4679a3f9c039d60a236c3274a
Mikasa33 <mikasa33@qq.com>
2023-08-04 16:21:57 +0800

    feat: 注册插件等

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createHead } from '@vueuse/head'
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+import './styles/main.less'
 
 import App from './App.vue'
 import router from './router'
+import { setupPinia } from './plugins/pinia'
+import { setupI18n } from './plugins/i18n'
+import { setupNprogress } from './plugins/nprogress'
 
 const app = createApp(App)
+const head = createHead()
 
-app.use(createPinia())
+setupPinia(app)
+setupI18n(app)
 app.use(router)
+app.use(head)
+setupNprogress(router)
 
 app.mount('#app')
