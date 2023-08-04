72999825018b095640c128806d7db4416bfb1278
Mikasa33 <mikasa33@qq.com>
2023-08-04 17:50:06 +0800

    feat: 添加样式

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig, presetAttributify, presetIcons, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from 'unocss'
 
 export default defineConfig({
-  shortcuts: [],
+  shortcuts: [
+    ['flex-center', 'flex items-center justify-center'],
+    ['flex-x-center', 'flex justify-center'],
+    ['flex-y-center', 'flex items-center'],
+    ['transition-base', 'transition duration-300'],
+    ['wh-full', 'w-full h-full'],
+  ],
   presets: [
     presetUno(),
     presetAttributify(),
