d51ffa3af78bad6aafa096f0a5464624ffbe89a6
Mikasa33 <mikasa33@qq.com>
2023-08-04 18:21:11 +0800

    feat: 增加 mock 功能

--- vite.config.ts
+++ vite.config.ts
@@ -1,69 +1,83 @@
 import { URL, fileURLToPath } from 'node:url'
 import path from 'node:path'
+import process from 'node:process'
 
-import { defineConfig } from 'vite'
+import type { ConfigEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import VueI18n from '@intlify/unplugin-vue-i18n/vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { viteMockServe } from 'vite-plugin-mock'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config
-export default defineConfig({
-  plugins: [
-    // https://vuejs.org
-    vue(),
+export default ({ command, mode }: ConfigEnv) => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
 
-    // https://github.com/vitejs/vite-plugin-vue/plugin-vue-jsx
-    vueJsx(),
+  return defineConfig({
+    plugins: [
+      // https://vuejs.org
+      vue(),
 
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        '@vueuse/core',
-        '@vueuse/head',
-        'pinia',
-      ],
-      dts: 'types/auto-imports.d.ts',
-      dirs: [
-        'src/composables',
-        'src/hooks',
-        'src/stores',
-      ],
-    }),
+      // https://github.com/vitejs/vite-plugin-vue/plugin-vue-jsx
+      vueJsx(),
 
-    // https://github.com/antfu/unplugin-vue-components
-    Components({
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'types/components.d.ts',
-    }),
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'vue-i18n',
+          '@vueuse/core',
+          '@vueuse/head',
+          'pinia',
+        ],
+        dts: 'types/auto-imports.d.ts',
+        dirs: [
+          'src/composables',
+          'src/hooks',
+          'src/stores',
+        ],
+      }),
 
-    // https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n
-    VueI18n({
-      runtimeOnly: true,
-      compositionOnly: true,
-      fullInstall: true,
-      include: [path.resolve(__dirname, 'locales/**')],
-    }),
+      // https://github.com/antfu/unplugin-vue-components
+      Components({
+        extensions: ['vue', 'md'],
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: 'types/components.d.ts',
+      }),
 
-    // https://github.com/webfansplz/vite-plugin-vue-devtools
-    VueDevTools(),
+      // https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n
+      VueI18n({
+        runtimeOnly: true,
+        compositionOnly: true,
+        fullInstall: true,
+        include: [path.resolve(__dirname, 'locales/**')],
+      }),
 
-    // https://github.com/antfu/unocss
-    UnoCSS(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // https://github.com/webfansplz/vite-plugin-vue-devtools
+      VueDevTools(),
+
+      // https://github.com/vbenjs/vite-plugin-mock
+      viteMockServe({
+        mockPath: 'mock',
+        enable: env.VITE_USE_MOCK === 'true',
+      }),
+
+      // https://github.com/antfu/unocss
+      UnoCSS(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    port: 3333,
-  },
-})
+    server: {
+      port: 3333,
+    },
+  })
+}
