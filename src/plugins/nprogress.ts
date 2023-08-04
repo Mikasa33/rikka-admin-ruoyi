e2526b6100fdc3ba01e3c73736b522f57ad63c36
Mikasa33 <mikasa33@qq.com>
2023-08-04 16:21:01 +0800

    feat: 添加插件

---
+++ src/plugins/nprogress.ts
@@ -0,0 +1,13 @@
+import NProgress from 'nprogress'
+import type { Router } from 'vue-router'
+
+export function setupNprogress(router: Router) {
+  router.beforeEach((to, from) => {
+    if (to.path !== from.path)
+      NProgress.start()
+  })
+
+  router.afterEach(() => {
+    NProgress.done()
+  })
+}
